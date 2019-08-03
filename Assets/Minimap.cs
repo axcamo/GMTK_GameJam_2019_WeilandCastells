dd01973129c069b6048fadc57eda33018f60fa12
BetaLemon <alexwelo@gmx.com>
2019-08-03 18:49:21 +0200

    Added minimap

---
+++ Assets/Minimap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+    }
+}
