8f35775159c0f9403433952203aa2dfd53885733
BetaLemon <alexwelo@gmx.com>
2019-08-03 15:55:00 +0200

    Added particles to bullets + test level

---
+++ Assets/EnterAreaTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterAreaTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform enemiesContainer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Bla");
+        if (other.CompareTag("Player"))
+        {
+            EnableEnemies();
+        }
+    }
+
+    void EnableEnemies()
+    {
+        for (int i = 0; i < enemiesContainer.childCount; ++i)
+        {
+            enemiesContainer.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+
+    void DisableEnemies()
+    {
+        for (int i = 0; i < enemiesContainer.childCount; ++i)
+        {
+            enemiesContainer.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+}
