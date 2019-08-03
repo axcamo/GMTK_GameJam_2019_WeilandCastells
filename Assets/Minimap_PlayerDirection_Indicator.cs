fcfba60fdfad82c84f906996d3e43f0c0d97c3f2
BetaLemon <alexwelo@gmx.com>
2019-08-03 20:14:35 +0200

    Merge2

---
+++ Assets/Minimap_PlayerDirection_Indicator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap_PlayerDirection_Indicator : MonoBehaviour
+{
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = Camera.main;
+    }
+    private void LateUpdate()
+    {
+        float angle = camera.transform.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(0, angle, 0);
+    }
+}
