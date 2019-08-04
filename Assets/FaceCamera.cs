36ffcef2b3eb4147fff764322edf30ecdfa86336
BetaLemon <alexwelo@gmx.com>
2019-08-04 16:38:02 +0200

    Added names to titlescreen and a tutorial

---
+++ Assets/FaceCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform camera;
+
+    private void Start()
+    {
+        camera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.forward = camera.forward.normalized;
+    }
+}
---
+++ Assets/TooltipManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TooltipManager : MonoBehaviour
+{
+    [SerializeField] private float fadeSpeed = 1f;
+
+    [SerializeField] private TextMeshPro welcomeText;
+    [SerializeField] private TextMeshPro controlsText;
+    [SerializeField] private TextMeshPro walkText;
+    [SerializeField] private GameObject startColumn;
+
+    private void Start()
+    {
+        StartCoroutine("Tutorial");
+    }
+
+    IEnumerator Tutorial()
+    {
+        StartCoroutine("FadeTextIn", welcomeText);
+        yield return new WaitForSeconds(10);
+        StartCoroutine("FadeTextOut", welcomeText);
+
+        StartCoroutine("FadeTextIn", controlsText);
+        yield return new WaitForSeconds(12);
+        StartCoroutine("FadeTextOut", controlsText);
+
+        startColumn.SetActive(true);
+
+        StartCoroutine("FadeTextIn", walkText);
+        yield return new WaitForSeconds(12);
+        StartCoroutine("FadeTextOut", walkText);
+    }
+
+    IEnumerator FadeTextIn(TextMeshPro text)
+    {
+        float alpha = 0;
+        while (text.color.a < 1f)
+        {
+            alpha += fadeSpeed * Time.deltaTime;
+            text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeTextOut(TextMeshPro text)
+    {
+        float alpha = 1;
+        while (text.color.a > 0f)
+        {
+            alpha -= fadeSpeed * Time.deltaTime;
+            text.color = new Color(text.color.r, text.color.g, text.color.b, alpha);
+            yield return null;
+        }
+    }
+}
